e23fcbab58fc5126bcdb86ccc3585b467ca45d02
Hrishiraj Sawan <hrishiraj102@gmail.com>
2023-03-05 19:21:18 +0530

    Logo updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,8 @@ function Header() {
             columnSpacing={{ xs: 1, sm: 2, md: 3 }}
             item xs={12} sm={6}>
 
-            <Grid>
+            <Grid sx={{display:"flex",flexDirection:"row",justifyContent:"center"}}>
+                <img style={{width:"100px",heigh:"100px",marginRight:"11px",marginTop:20,position:"relative"}} src='./DraculaLogo.jpeg' alt="Logo"/>
                 <Typography variant="h2" sx={{ color: "white", fontFamily: "sans-serif" ,pt:6,fontSize:"auto",fontWeight:400}}>DRACULA</Typography>
             </Grid>
             <NavBar />
